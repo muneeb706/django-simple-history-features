7c72ea009dbf41eb9ce87102992a7a4de7e2e826
Muneeb Shahid <muneeb706@gmail.com>
2023-06-24 17:48:44 -0700

    feat(inherit): Sample code for inherit feature

--- inherit/models.py
+++ inherit/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from simple_history.models import HistoricalRecords
 
+# Docs: https://django-simple-history.readthedocs.io/en/latest/historical_model.html?highlight=inherit#allow-tracking-to-be-inherited
+
 class BaseModel(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     last_updated_date = models.DateTimeField(auto_now=True)
@@ -8,6 +10,4 @@ class BaseModel(models.Model):
   
 class ChildModel(BaseModel):
     title = models.CharField(max_length=255, blank=False, null=False)
-    class Meta:
-       db_table = "child_model"
 
